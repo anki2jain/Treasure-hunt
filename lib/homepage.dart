175bad99641fc9a5de1c87f78df5ae14fdf6d3d4
Ankit <jain2anki@gmail.com>
2019-10-01 01:14:15 +0530

    splash navbar rules

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:treasurehunt/map.dart';
 import 'package:treasurehunt/rules.dart';
 
 class Homepage extends StatefulWidget {
@@ -9,34 +10,49 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   int currentPage = 0;
+  Widget page;
   Color backcolor = Color(0xFF000020);
   Color color1 = Color(0xFFD4AF37);
-   Color color = Color(0xFFFFDF00);
+  Color color = Color(0xFFFFDF00);
+  Rules rules = new Rules();
+  Mapview map = new Mapview();
+  @override
+  void initState() {
+    page = rules;
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: FancyBottomNavigation(
-        circleColor: color1,
-        textColor: color,
-        inactiveIconColor: color1,
-        barBackgroundColor: backcolor,
-        tabs: [
-          TabData(iconData: Icons.help_outline, title: "Help"),
-          TabData(iconData: Icons.games, title: "Treasure"),
-          TabData(iconData: Icons.map, title: "Map")
-        ],
-        onTabChangedListener: (position) {
-          setState(() {
-            currentPage = position;
-          });
-        },
-      ),
-      backgroundColor: backcolor,
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(child: Rules()),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: backcolor,
+          leading: Icon(Icons.info_outline, color: color1),
+        ),
+        bottomNavigationBar: FancyBottomNavigation(
+          circleColor: color1,
+          textColor: color,
+          inactiveIconColor: color1,
+          barBackgroundColor: backcolor,
+          tabs: [
+            TabData(iconData: Icons.help_outline, title: "Help"),
+            TabData(iconData: Icons.games, title: "Treasure"),
+            TabData(iconData: Icons.map, title: "Map")
+          ],
+          onTabChangedListener: (position) {
+            setState(() {
+              currentPage = position;
+              if (currentPage == 0) {
+                page = rules;
+              }
+              if (currentPage == 1) {
+                page = map;
+              }
+            });
+          },
+        ),
+        backgroundColor: backcolor,
+        body: page);
   }
 }
---
+++ lib/map.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mapview extends StatefulWidget {
+  @override
+  _MapviewState createState() => _MapviewState();
+}
+
+class _MapviewState extends State<Mapview> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("data"),
+    );
+  }
+}
